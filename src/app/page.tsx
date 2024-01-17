c6f8e4e8ba5bc7d338e417b1712b8274ab23aa1f
toluOjo <ojotolulope137@gmail.com>
2024-01-17 13:27:41 +0100

    conditionally render page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@
 import { useState } from 'react';
 
 export default function Home() {
-
-
+const [signIn,setSignIn]=useState(false);
+const togglePage =()=>{
+  setSignIn(!signIn)
+}
   const [form, setForm] = useState({
     name: "",
     email: "",
@@ -26,7 +28,7 @@ hi
       </div>
    
     <div className="bg-slate-200  w-1/4 rounded-lg flex flex-col justify-center items-center gap-4 h-screen">
-   <h1 className="mt-4">SIGN UP</h1>
+   {signIn ? <><h1 className="mt-4">SIGN UP</h1>
     <form onSubmit={handleSubmit} className="flex flex-col w-5/6 my-1">
        <label htmlFor="name">Enter name</label>
        <input
@@ -65,9 +67,39 @@ hi
          Enter
        </button>
      </form>
-     Already have an account? sign in.
-   </div> 
-
+     Already have an account?<span onClick ={togglePage}>sign in.</span>
+     </>:
+     
+     <><h1 className="mt-4">SIGN IN</h1>
+    <form onSubmit={handleSubmit} className="flex flex-col w-5/6 my-1">
+    
+       <label htmlFor="email">Enter Email</label>
+       <input
+        className="h-10 px-2 bg-inherit border-b rounded-none border-gray-500"
+         type="email"
+         name="email"
+         onChange={handleChange}
+       />
+       <label htmlFor="password">Enter Password</label>
+       <input
+         className="h-10 px-2 bg-inherit  border-b rounded-none border-gray-500"
+         type="password"
+         name="password"
+         id=""
+         onChange={handleChange}
+       />
+       <button
+         type="submit"
+         className="bg-slate-500 rounded-xl w-1/3 mx-auto py-2 my-4"
+       >
+         Enter
+       </button>
+     </form>
+     New User? <span onClick ={togglePage}> kindly sign up.</span>
+     </>
+   }
+   </div>
+   
     </main>
   )
 }
